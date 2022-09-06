871921bd3aa428b03d26245e6b3a6bfbec45d81e
Taregnaeem <targhafeez@gmail.com>
2022-09-06 23:26:12 +0300

    11:26 Commit

--- maven-samples/single-module/src/test/java/com/example/TestGreeter.java
+++ maven-samples/single-module/src/test/java/com/example/TestGreeter.java
@@ -48,6 +48,12 @@ public class TestGreeter {
     assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
   }
 
+  @Test
+  public void greetShouldIncludeGreetingMessage() {
+    String someone = "World, This is a tion";
+
+    assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
+  }
 
   
 
